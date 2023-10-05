4b59d559120190bc5b3e14f6e5c700a734bb3a6e
Denis <98878339+MisterDenisZubrin@users.noreply.github.com>
2023-10-05 21:55:17 +0300

    Новая версия калькулятора, планшетная версия сайта

---
+++ scss/js/calculator.js
@@ -0,0 +1,127 @@
+"use strict";
+
+// Button activator logic
+
+function toggleClass(target, targetClass) {
+  target.classList.toggle(targetClass);
+}
+
+const calculator = document.querySelector(".calculator");
+document
+  .querySelector("#activator")
+  .addEventListener("click", () =>
+    toggleClass(calculator, "calculator_active")
+  );
+
+// Calculator
+
+const screen = document.querySelector("#screen");
+const buttons = document.querySelectorAll(".calculator__button");
+
+let firstNumber = "",
+  memory = "",
+  operator = "";
+
+function setValue(content) {
+  if (
+    content === "+" ||
+    content === "-" ||
+    content === "*" ||
+    content === "/"
+  ) {
+    // Если до этого вводились операции, то нужно досчитать их. Иначе просто сохраняем первое число после ввода оператора
+    if (operator !== "") {
+      calculate(firstNumber, +memory, operator);
+      memory = "";
+    } else {
+      // Если я продолжаю после =, то вспомнить последний результат
+      if (!firstNumber) {
+        firstNumber = +memory;
+        memory = "";
+      }
+    }
+    operator = content;
+    screen.value += content;
+  } else {
+    // Защита от 0 в начале числа
+    if (firstNumber === "" && content === "0" && memory === "0") {
+      firstNumber = 0;
+      screen.value = 0;
+      memory = "0";
+    } else {
+      if (!(content === "0" && memory === "0")) {
+        if (memory === "0" && content !== "0") {
+          memory = content;
+          screen.value = screen.value.slice(0, -1) + memory;
+        } else {
+          screen.value += content;
+          memory += content;
+        }
+      }
+    }
+  }
+}
+
+function sum(num1, num2) {
+  return +num1 + +num2;
+}
+function substract(num1, num2) {
+  return +num1 - +num2;
+}
+function multyply(num1, num2) {
+  return +num1 * +num2;
+}
+function divide(num1, num2) {
+  return +num1 / +num2;
+}
+
+function setResult(value) {
+  screen.value = value;
+  firstNumber = value;
+  memory = "";
+  operator = "";
+}
+
+function reset() {
+  screen.value = "";
+  firstNumber = "";
+  memory = "";
+  operator = "";
+}
+
+function calculate(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      setResult(sum(num1, num2));
+      break;
+    case "-":
+      setResult(substract(num1, num2));
+      break;
+    case "*":
+      setResult(multyply(num1, num2));
+      break;
+    case "/":
+      setResult(divide(num1, num2));
+      break;
+  }
+}
+
+buttons.forEach((button) => {
+  const content = button.textContent;
+  if (button.id === "enter") {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      calculate(firstNumber, memory, operator);
+    });
+  } else if (button.id === "clear") {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      reset();
+    });
+  } else {
+    button.addEventListener("click", (e) => {
+      e.preventDefault();
+      setValue(content);
+    });
+  }
+});
